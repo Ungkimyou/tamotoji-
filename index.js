b66aeb1f3bda87623fb01c2fb92c9b406dbbc9a0
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-10 19:22:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -125,6 +125,34 @@ client.on("message", async message => {
     })
 };
    
+   if(command === "poll") {
+ 	if(message.author.id !== botconfig.ownerID) return;
+  if (!args.join(' ')) return message.channel.send('Usage: poll <title>').then(msg => msg.delete({timeout: 10000}));
+  
+  const embed = new Discord.RichEmbed()
+    .setTitle(args.join(' '))
+    .setColor('RANDOM')
+    const pollTitle = await message.channel.send({ embed });
+      await pollTitle.react(`👍`);
+      await pollTitle.react(`❤`);
+      await pollTitle.react(`☺`);
+  
+    const filter = (reaction) => reaction.emoji.name === '👍';
+    const collector = pollTitle.createReactionCollector(filter, { time: 15000 });
+      collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+      collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+  
+    const filter1 = (reaction) => reaction.emoji.name === '❤';
+    const collector1 = pollTitle.createReactionCollector(filter1, { time: 15000 });
+      collector1.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+      collector1.on('end', collected => console.log(`Collected ${collected.size} items`));
+
+    const filter2 = (reaction) => reaction.emoji.name === '☺';
+    const collector2 = pollTitle.createReactionCollector(filter2, { time: 15000 });
+      collector2.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+      collector2.on('end', collected => console.log(`Collected ${collected.size} items`));
+};
+
 });
 
 client.login(botconfig.token);

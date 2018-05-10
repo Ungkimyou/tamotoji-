c261bfbaf435719b775a80d95c93bc74b2911016
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-10 11:52:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -124,6 +124,25 @@ client.on("message", async message => {
       message.channel.send('```' + data + '```')
     })
 };
+   
+     if(command === "magik") {
+      let target = message.mentions.users.first() || message.author;
+      let wait = await message.channel.send('Adding The Magik...')
+  
+      let userAvatar = (target.displayAvatarURL);
+      if (['jpg', 'jpeg', 'gif', 'png', 'webp'].some(x => args.join(' ').includes(x))) {
+          userAvatar = args.join(' ').replace(/gif|webp/g, 'png')
+      }
+  
+      let res = await snek.get(`https://discord.services/api/magik?url=${userAvatar}`)
+  
+              await wait.delete()
+  
+              const magikEmbed = new Discord.RichEmbed() 
+                  .setImage(`https://discord.services/api/magik?url=${userAvatar}`); 
+              return message.channel.send(magikEmbed).message.delete(1000);
+  
+  }
 
 });
 

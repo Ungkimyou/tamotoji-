e69397b2e8e3313cfc579803f31dd5041b1efc2a
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-10 09:59:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,14 +42,13 @@ client.on("message", async message => {
 
     if(command === "gif") {
        if (message.author.bot) return;
-       if (message.channel.type == "dm") return;
        if (!args[0]) return message.channel.send("`"+PREFIX+"gif <gname>`");
 
        gifSearch.random(args[0]).then(
        gifUrl => {
         let randomcolor = ((1 << 24) * Math.random() | 0).toString(16)
         var embed = new Discord.RichEmbed()
-            .setColor(`#${randomcolor}`)
+            .setColor(`RANDOM`)
             .setImage(gifUrl)
         message.channel.send(embed);
         message.delete();
@@ -107,8 +106,8 @@ client.on("message", async message => {
 }
 
     if(command === "ascii") {
-  message.delete(5000);
-  message.react("✅");
+  	if(message.author.id !== botconfig.ownerID) return;
+  message.delete()
   if(args.join(' ').length > 14) return message.channel.send('Only 14 characters are admitted!') 
   if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000})); 
     figlet(args.join(' '), (err, data) => {

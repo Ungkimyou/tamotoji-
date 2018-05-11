e40514b2e0729412ce143f40dbbba8a41ff9ecb7
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-11 14:26:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("message", async message => {
     .setAuthor("Discord Server :", message.author.avatarURL)
     .setDescription('📌This is My Discord Server : Please Join and Support Now Thank For Support Alway ! 🎉')
     .addBlankField()
-    .setURL("tamotoji.tk")
+    .setURL()
     .addField('🌎 Discord Website :', "[Click Here](tamotoji.tk) View Here ! 💌")
     .addField('🔗 Discord Link :', "[Clike Here](https://discord.gg/7mS9GEY) Join Server Now ! 🚀 ")
     .setImage('https://cdn.discordapp.com/attachments/364284401497931786/443691032048041984/Discord.logo.png')
